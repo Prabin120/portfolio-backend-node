e39afdeb3d1fa48df28950a031d0a76953b22511
“Prabin <prabin@roboprenr.com>
2024-02-25 14:31:49 +0530

    vercel is uploaded

--- controllers/gmail.js
+++ controllers/gmail.js
@@ -5,7 +5,7 @@ dotenv.config();
 async function mailSender(data){
     const transporter = nodemailer.createTransport({
         service:"gmail",
-        host: 'smtp.gmail.com',  
+        host: 'smtp.gmail.com',
         port: 587,
         secure: false,
         auth: {
