030a94704ac5640ef97566fb4fca9ae079e09eb0
“Prabin <prabin@roboprenr.com>
2024-02-25 13:27:22 +0530

    Mail Sending func is added

--- middlewares/gmail.js
+++ middlewares/gmail.js
@@ -1,15 +1,27 @@
 const nodemailer = require('nodemailer')
+const dotenv = require('dotenv')
+dotenv.config();
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.MAIL_USERNAME,
-      pass: process.env.MAIL_PASSWORD,
-      clientId: process.env.OAUTH_CLIENTID,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH_REFRESH_TOKEN
-    }
-});
+async function mailSender(data){
+    const transporter = nodemailer.createTransport({
+        service:"gmail",
+        host: 'smtp.gmail.com',  
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.MAIL_USERNAME,
+            pass: process.env.MAIL_PASSWORD,
+        }
+    });
+    const html = `<p>Name: ${data.name}</p> <p>Email: ${data.email}</p><p>Message: ${data.message}</p>`
+    const info = await transporter.sendMail({
+        from: "Prabin Sharma <sharmaprabin2233@gmail.com>",
+        to: "prabinsharma120@gmail.com",
+        subject: "Portfolio Form Submission",
+        html: html
+    })
+    // console.log("Message sent",info);
+    return info;
+}
 
-module.exports = transporter;
+module.exports = mailSender;
--- routes/contact.js
+++ routes/contact.js
@@ -2,11 +2,19 @@ const express = require("express");
 const {SocialLink,ContactForm } = require('../models/contact')
 const router = express.Router();
 
+const mailSender = require('../middlewares/gmail')
+
 router.get("/social-link/:name",async (req,res)=>{
     const name = req.params.name;
     const link = await SocialLink.findOne({name});
     res.status(200).json(link);
 })
+
+// router.get("/",async(req,res)=>{
+//     const info = await mailSender()
+//     .catch(e=>console.log(e));
+//     return res.json(info);
+// })
 
 router.post("/",async(req,res)=>{
     const {name,email,message} = req.body;
@@ -18,6 +26,9 @@ router.post("/",async(req,res)=>{
         email:email,
         message:message
     })
+    const info = await mailSender(data)
+    .catch(e=>console.log(e));
+    // return res.json(info);
     return res.status(200).send("Thank you for the message");
 })
 module.exports = router;
